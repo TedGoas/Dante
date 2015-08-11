61234cc6c223ae12a4fa8e3d76face8e2485fecc
Ted Goas <ted@tedgoas.com>
2015-08-10 22:16:56 -0400

    Fixes Issue #8
    Fix background on mobile homepage

--- site/templates/home.php
+++ site/templates/home.php
@@ -1,10 +1,9 @@
 <?php snippet('header') ?>
-<div class="big-ted">
-	<div class="home-intro">
-		<h1 class="home-intro--title">I'm a reliable designer &amp; front-end developer working on websites, web apps, and HTML emails. I design things to be understandable, credible, easy to navigate, and fast.</h1>
-		<p>I'm trying to create work that is cool enough to show my friends and honest enough to show my parents.</p>
-		<p>When I'm not working on the web, I enjoy spending time with my wife and son, live music, snowboarding, hockey, soccer, Newcastle Ale, and quoting Troy McClure.</p>
-		<p><img src="/assets/images/sig.png" alt="Ted Goas" width="210" height="60" class="signature"></p>
-	</div>
+<div class="home-intro">
+	<h1 class="home-intro--title">I'm a reliable designer &amp; front-end developer working on websites, web apps, and HTML emails. I design things to be understandable, credible, easy to navigate, and fast.</h1>
+	<p>I'm trying to create work that is cool enough to show my friends and honest enough to show my parents.</p>
+	<p>When I'm not working on the web, I enjoy spending time with my wife and son, live music, snowboarding, hockey, soccer, Newcastle Ale, and quoting Troy McClure.</p>
+	<p><img src="/assets/images/sig.png" alt="Ted Goas" width="210" height="60" class="signature"></p>
 </div>
+<div class="big-ted"></div>
 <?php snippet('footer') ?>
