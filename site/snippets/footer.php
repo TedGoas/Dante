ef6e4a442d0f07547489ef8d4d035478bef01077
Ted Goas <ted@tedgoas.com>
2015-11-27 12:35:54 -0500

    Resolves #13
    No sure why I ever did this.

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -3,14 +3,12 @@
 	<script src="/assets/js/jquery-1.11.3.min.js"></script>
 	<script src="/assets/js/tedgoas.js"></script>
 	<script>
-	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-	  ga('create', 'UA-3248378-1', 'auto');
-	  ga('send', 'pageview');
-	  ga('require', 'displayfeatures');
-	  ga('require', 'linkid', 'linkid.js');
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+		ga('create', 'UA-3248378-1', 'auto');
+		ga('send', 'pageview');
 	</script>
 </body>
 </html>
