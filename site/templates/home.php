ebe218970e38cccb3cf04fd3c71ed86ba9753906
Ted Goas <ted@tedgoas.com>
2016-02-08 19:01:37 -0500

    New Homepage Copy!
    Resolves #18

--- site/templates/home.php
+++ site/templates/home.php
@@ -1,8 +1,8 @@
 <?php snippet('header') ?>
 <div class="home-intro">
-	<h1 class="home-intro--title">I'm a reliable designer &amp; front-end developer working on websites, web apps, and HTML emails.</h1>
-	<p class="home-intro-p">I design things to be more understandable, credible and valuable. My goal is to create work that is cool enough to show my friends and honest enough to show my parents.</p>
-	<p class="home-intro-p">When I'm not working on the web, I enjoy spending time with my wife and kids, live music, snowboarding, hockey, soccer, Newcastle Ale, and Troy McClure quotes.</p>
+	<h1 class="home-intro--title">I’m a reliable designer and front-end developer who makes websites, applications, and HTML emails.</h1>
+	<p class="home-intro-p">I believe design is about taking the complex and chaotic and making it accessible to people, so I design things to be understandable, readable, responsive, and as fast as possible. Since 2001, I’ve been trying to create work that is cool enough to show my friends and honest enough to show my parents.</p>
+	<p class="home-intro-p">When I'm not working, I enjoy family time with my wife and kids, live music, snowboarding, hockey, soccer, Newcastle Ale, and Troy McClure quotes.</p>
 	<p>
 		<object type="image/svg+xml" data="/assets/images/signature.svg" alt="Ted Goas">
 		    - Ted Goas
