608754d9d9c981ae646884ed3c8eb46d62f6727e
Ted Goas <ted@tedgoas.com>
2016-02-16 21:17:28 -0500

    Updated Availability Graph / Page
    Refreshed content
    Work towards #2
    Resolves #32

--- site/templates/contact.php
+++ site/templates/contact.php
@@ -1,26 +1,43 @@
 <?php snippet('header') ?>	
-<div class="outside body article">
+<div class="outside body">
 	
 	<div class="inside page-title">
 		<h1 class="page-title-h1"><?php echo $page->title()->html() ?></h1>
 	</div>
 	
 	<div class="inside">
-		<article>
-			<div class="uk-grid uk-grid-large">
-				<div class="uk-width-1-2">
-					<?php echo $page->text()->kirbytext() ?>
+		<div class="narrow-width availability">
+			<h2>Availability</h2>
+			<div class="av-graph">
+				<div class="av-graph-railing">
+					<div class="av-graph-fill">
+						<div class="caption">I'm fairly booked up, but let's talk!</div>
+					</div>
 				</div>
-				<div class="uk-width-1-2 availability">
-					<h2>Availability</h2>
-					<img src="/assets/images/availability-dial.png" alt="relatively unavailable" height="235" width="450" class="availability-dial">
+				<ul class="labels-ul">
+					<li class="labels-li labels-li-low">Mostly Unavailable</li>
+					<li class="labels-li labels-li-high">Relatively Available</li>
+				</ul>
+			</div>
+		</div>
+		<article>
+			<div class="uk-grid">
+				<div class="uk-width-1-3">
+					<p class="contact-primary">During the day, I'm a designer and front-end developer at Canfield Scientific in northern New&nbsp;Jersey.</p>
+				</div>
+				<div class="uk-width-1-3">
+					<p>If you're interested in <strong class="highlighted">freelance work</strong> and have something small or well-defined, let's chat. Please be aware that I already have a handful of freelance clients, personal projects, and my day job.</p>
+				</div>
+				<div class="uk-width-1-3">
+					<p>If you'd like to ask a question, talk shop, or just say hi, please drop me an email! I promise I'll reply as soon as I can.</p>
 				</div>
 			</div>
-			<div class="tatg">
-				<div class="hr loud"><hr></div>
-				<object type="image/svg+xml" data="/assets/images/email.svg" alt="hi spam bots" width="315" height="30">
+			<div class="narrow-width tatg">
+				<div class="hr"><hr></div>
+				<object type="image/svg+xml" data="/assets/images/email.svg" alt="hi spam bots" width="315" height="30" class="em">
 				    ted at tedgoas dot com
 				</object>
+			</div>
 		</article>
 	</div>
 	
