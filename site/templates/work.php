627a1190e8fd3d70bce98b283397ccd569c4fa1b
Ted Goas <ted@tedgoas.com>
2016-03-02 22:46:37 -0500

    Redesign Work Index on Narrow
    Resolves #35
    Also realigned design wide.

--- site/templates/work.php
+++ site/templates/work.php
@@ -15,25 +15,25 @@
 				</div>
 			</a>
 			<a href="/work/clinical-services/" class="work-list-item work-list-item--clinical effect-jazz">
+				<img src="/assets/images/work/clinical-services/index-thumb.jpg" width="600" height="350" alt="Clinical Services" class="work-list-item-img">
 				<div class="work-list-item-text">
 					<h2 class="work-item-title">Clinical Services</h2>
 					<p class="work-item-p">How I Learned to Design with Data</p>
 				</div>
-				<img src="/assets/images/work/clinical-services/index-thumb.jpg" width="600" height="350" alt="Clinical Services" class="work-list-item-img">
 			</a>
 			<a href="/work/view-my-consult/" class="work-list-item work-list-item--vmc">
+				<img src="/assets/images/work/vmc/index-thumb.jpg" width="600" height="350" alt="View My Consult" class="work-list-item-img">
 				<div class="work-list-item-text">
 					<h2 class="work-item-title">View My Consult</h2>
 					<p class="work-item-p">Planning, prototyping, and launching Canfield's first telemedicine app</p>
 				</div>
-				<img src="/assets/images/work/vmc/index-thumb.jpg" width="600" height="350" alt="View My Consult" class="work-list-item-img">
 			</a>
 			<a href="/work/canfield-misc/" class="work-list-item work-list-item--canfieldmisc">
+				<img src="/assets/images/work/canfield-misc/index-thumb.jpg" width="600" height="350" alt="Misc Design at Canfield" class="work-list-item-img">
 				<div class="work-list-item-text">
 					<h2 class="work-item-title">Misc. Canfield Projects</h2>
 					<p class="work-item-p">Small to Medium Size Projects at Canfield</p>
 				</div>
-				<img src="/assets/images/work/canfield-misc/index-thumb.jpg" width="600" height="350" alt="Misc Design at Canfield" class="work-list-item-img">
 			</a>
 		</div>
 	</div>
@@ -45,18 +45,18 @@
 	<div class="inside">
 		<div class="work-list">
 			<a href="/work/html-email-design/" class="work-list-item work-list-item--email">
+				<img src="/assets/images/work/html-email-design/index-thumb.jpg" width="600" height="350" alt="Email Design" class="work-list-item-img">
 				<div class="work-list-item-text">
 					<h2 class="work-item-title">Email Design</h2>
 					<p class="work-item-p">I help folks level up their email design and marketing</p>
 				</div>
-				<img src="/assets/images/work/html-email-design/index-thumb.jpg" width="600" height="350" alt="Email Design" class="work-list-item-img">
 			</a>
 			<a href="/work/web-ui-design" class="work-list-item work-list-item--webui">
+				<img src="/assets/images/work/web-ui-design/index-thumb.jpg" width="600" height="350" alt="Web & Interface Design" class="work-list-item-img">
 				<div class="work-list-item-text">
 					<h2 class="work-item-title">Web &amp; UI Design</h2>
 					<p class="work-item-p">I help companies, startups, and local business with website and interface design</p>
 				</div>
-				<img src="/assets/images/work/web-ui-design/index-thumb.jpg" width="600" height="350" alt="Web & Interface Design" class="work-list-item-img">
 			</a>
 		</div>
 	</div>
