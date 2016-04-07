5d5aecdaa28352cf4f97588bf359ca649d50ce46
Ted Goas <ted@tedgoas.com>
2016-04-06 22:09:39 -0400

    Wordsmith "What I Do" to focus on career path
    Resolves #43

--- site/templates/what-i-do.php
+++ site/templates/what-i-do.php
@@ -20,40 +20,41 @@
 	</div>
 	
 	<div class="inside">		
-		<div class="uk-grid">
+		<div class="uk-grid uk-grid-large">
 			<div class="uk-width-1-2">
 				<div class="what-i-do-summary">
-					<?php echo $page->text()->kirbytext() ?>
+					<p>I'm a highly adaptable designer best suited in teams where everyone can wear multiple hats. From scoping requirements to planning content – through design and development – collaborating with clients and co-workers is paramount. I prefer working with others and sharing regular updates rather than disappearing for long stretches and emerging with big surprises.</p>
 				</div>
 			</div>
-			<div class="uk-width-1-4">
-	
-				<div class="skills-detail-list">
-					<h4 class="skills-detail-title skills-detail-title--good">I Can Wrangle This Stuff</h4>
-					<ul class="skills-detail-ul skills-detail-ul--even">
-						<li class="skills-detail-li">Information Architecture</li>
-						<li class="skills-detail-li">Mobile Design (iOS)</li>
-						<li class="skills-detail-li">Email Marketing</li>
-						<li class="skills-detail-li">Website Performance</li>
-						<li class="skills-detail-li">Source Control (Git, SVN)</li>
-						<li class="skills-detail-li">Copywriting</li>
-						<li class="skills-detail-li">SEO &amp; Findability</li>
-					</ul>
+			<div class="uk-width-1-2">
+				<div class="what-i-do-summary">
+					<p>My goal is to be a designer-manager hybrid. I enjoy hands-on work and having my own projects. On top of that, I’d like to mentor or manage a small team, making sure they’re happy, inspired, and successfully delivering for their clients. I’m interested in facilitating critiques, discussing new processes, designing exercises, helping with sales leads, and any other problems whether code or people.</p>
 				</div>
 			</div>
-			<div class="uk-width-1-4">
-				<div class="skills-detail-list">
-					<h4 class="skills-detail-title skills-detail-title--bad">This Stuff, Not So Much</h4>
-					<ul class="skills-detail-ul skills-detail-ul--odd">
-						<li class="skills-detail-li">Back-End Programming</li>
-						<li class="skills-detail-li">Print Design</li>
-						<li class="skills-detail-li">Logo Design</li>
-						<li class="skills-detail-li">Illustration</li>
-						<li class="skills-detail-li">SEO / PPC Campaigns</li>
-						<li class="skills-detail-li">Social Media Mgmt</li>
-					</ul>
-				</div>
-			</div>
+		</div>
+		
+		<div class="skills-detail-list skills-detail-list--good">
+			<h4 class="skills-detail-title skills-detail-title--good">I Can Wrangle This Stuff</h4>
+			<ul class="skills-detail-ul">
+				<li class="skills-detail-li skills-detail-li--good">Information Architecture</li>
+				<li class="skills-detail-li skills-detail-li--good">Mobile Design (iOS)</li>
+				<li class="skills-detail-li skills-detail-li--good">Email Marketing</li>
+				<li class="skills-detail-li skills-detail-li--good">Website Performance</li>
+				<li class="skills-detail-li skills-detail-li--good">Source Control (Git, SVN)</li>
+				<li class="skills-detail-li skills-detail-li--good">Copywriting</li>
+				<li class="skills-detail-li skills-detail-li--good">SEO &amp; Findability</li>
+			</ul>
+		</div>
+		<div class="skills-detail-list skills-detail-list--bad">
+			<h4 class="skills-detail-title skills-detail-title--bad">This Stuff, Not So Much</h4>
+			<ul class="skills-detail-ul">
+				<li class="skills-detail-li skills-detail-li--bad">Back-End Programming</li>
+				<li class="skills-detail-li skills-detail-li--bad">Print Design</li>
+				<li class="skills-detail-li skills-detail-li--bad">Logo Design</li>
+				<li class="skills-detail-li skills-detail-li--bad">Illustration</li>
+				<li class="skills-detail-li skills-detail-li--bad">SEO / PPC Campaigns</li>
+				<li class="skills-detail-li skills-detail-li--bad">Social Media Mgmt</li>
+			</ul>
 		</div>
 	
 	</div>
