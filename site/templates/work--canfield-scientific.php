56f536f7ca249b9e87055e449cc2178b71001561
Ted Goas <ted@tedgoas.com>
2018-09-17 22:08:23 -0400

    Figures for Working @ Canfield

--- site/templates/work--canfield-scientific.php
+++ site/templates/work--canfield-scientific.php
@@ -2,25 +2,25 @@
 <article>
 	<h1><?php echo $page->title()->html() ?></h1>
 	<p>As Canfield’s only designer, I worked on websites, apps, email marketing, and iOS apps. My day-to-day involved a mix of research, sketching, Photoshop, coding, writing, and meetings. I helped guide project strategy, worked directly with our clients (both internal and external), and lead product demos and design critiques.</p>
-	<figure class="outset my5">
-		<img src="http://via.placeholder.com/900x400">
-		<figcaption class="stuff">[Salary Calculator]</figcaption>
+	<figure class="my7">
+		<img src="/assets/images/work/canfield/careers.jpg" width="1000" height="800" alt="Canfield Careers microsite" class="bordered">
+		<figcaption class="stuff">Careers Microsite Concept</figcaption>
 	</figure>
 	<p>Much of my time was spent designing apps for clinical trials and doctors offices. Before joining Canfield, everything I’d worked on was consumer based. While some things translate well, in other respects designing for medical is a completely different ballgame. I wrote about my experiences, the good and the bad.</p>
-	<figure class="outset my5">
-		<img src="http://via.placeholder.com/900x400">
-		<figcaption class="stuff">[Derm Summit]</figcaption>
+	<figure class="my7">
+		<img src="/assets/images/work/canfield/derm-summit.jpg" width="1000" height="800" alt="conference site design" class="bordered">
+		<figcaption class="stuff">Conference site design</figcaption>
 	</figure>
 	<p>We didn’t have a formalized development process, so we often had major insights that required us to make large, sometimes destabilizing changes late in projects. Our milestones were usually tied to immovable dates like trade shows, so we were always prepared to ship a stable version of the product and watched closely as new clients began using our products.</p>
-	<figure class="outset my5">
-		<img src="http://via.placeholder.com/900x400">
-		<figcaption class="stuff">[Email Design]</figcaption>
+	<figure class="my7">
+		<img src="/assets/images/work/canfield/email-design.jpg" width="1000" height="800" alt="email designs">
+		<figcaption class="stuff">Various email designs</figcaption>
 	</figure>
 	<p>I was responsible for every email Canfield sent. We sent about marketing 20-25 emails a year, a mixture of promotional, loyalty, event, and digest emails. Our web products also send transactional emails for things like password reset and account confirmation. I work with stakeholders to design each email, code for all popular email clients (Litmus), and schedule deployment in our ESP or codebase, and track metrics.</p>
 	<p>[blog post about designing in a sea of engineering]</p>
-	<figure class="outset my5">
-		<img src="http://via.placeholder.com/900x400">
-		<figcaption class="stuff">[Corporate Website]</figcaption>
+	<figure class="my7">
+		<img src="/assets/images/work/canfield/wireframe-vmc.jpg" width="1000" height="500" alt="wireframe of telemedicine app" class="bordered">
+		<figcaption class="stuff">Early explorations and questions about a telemedicine app</figcaption>
 	</figure>
 	<h2>The Big Redesign</h2>
 	<p>One of the larger projects I worked on was the redesign of the company website. The website was personal to a lot people, and its redesign brought out a lot of emotions. Stakeholders were proud, opinionated, worried, territorial. Everyone had the same goal for the site, but a different opinion on how to get there. It was my job to capture everyone’s vision and balance it with the needs of the business.</p>
@@ -28,18 +28,18 @@
 		<p>I Was Asked to Lead, But Wasn't In Charge</p>
 	</blockquote>
 	<p>I regularly met with the heads of each department to understand their needs and worked directly with the company owner to mold these inputs into an accurate portrait of his company. I led countless design reviews and demos, organized feedback, and prioritized resulting work.</p>
-	<figure class="outset my5">
-		<img src="http://via.placeholder.com/900x400">
-		<figcaption class="stuff">The homepage alone went through over 10 iterations.</figcaption>
+	<figure class="my7">
+		<img src="/assets/images/work/canfield/canfieldsci.jpg" width="1000" height="500" alt="evolution of Canfield's homepage" class="bordered">
+		<figcaption class="stuff">A few iterations of <a href="https://www.canfieldsci.com/">Canfield's corporate home page</a></figcaption>
 	</figure>
 	<p>Many of Canfield’s products and services are specialized, so they didn’t always fit into shared templates very well. I was challenged to design flexible templates that could share <em>some</em> design patterns while allowing for one-off, art-directed design when necessary.</p>
 	<p>I developed a series of templates made up modular blocks that could be mixed and matched to create any product or service page.</p>
 	<h3>Navigating Conflict</h3>
 	<p>This was a high profile project within the company and emotions ran high. I was in the middle of a few tense meetings. I focused on asking lots of questions, listening, and keeping my own biases from leading the design in the wrong direction.</p>
 	<p>During the planning process, I asked each stakeholder what success looks like and noted their answer. We made small adjustments during the project, but having documented goals to reference avoided lots of churn. Ultimately each design we produced succeeded when it solved the problem, not because “everyone liked it.”</p>
-	<figure class="outset my5">
-		<img src="http://via.placeholder.com/900x400">
-		<figcaption class="stuff">caption</figcaption>
+	<figure class="my7">
+		<img src="/assets/images/work/canfield/canfieldsci2.jpg" width="1000" height="800" alt="Canfield's About landing page" class="bordered">
+		<figcaption class="stuff"><a href="https://www.canfieldsci.com/about/">Canfield's About page</a></figcaption>
 	</figure>
 	<h3>The Result</h3>
 	<p>The redesign was met with overwhelming positivity. Clients approached us at conferences, commending the site’s way-finding systems and refreshed content. Each area of the business was now accurately represented and stakeholders have more tools to maintain their area of the site.</p>
