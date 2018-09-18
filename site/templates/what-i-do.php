ea38a37785a8d815ba50b65ec8821d43c2a5c876
Ted Goas <ted@tedgoas.com>
2018-09-17 20:16:27 -0400

    What I Do = done

--- site/templates/what-i-do.php
+++ site/templates/what-i-do.php
@@ -6,15 +6,16 @@
 			<p>I'm a highly adaptable designer best suited in teams where everyone can wear multiple hats. From scoping requirements to planning content – through design and development – collaborating with clients and co-workers is paramount. I prefer designing with others and sharing regular updates rather than working in isolation for long stretches.</p>
 			<p>For the past several years, I've become comfortable in a <strong>double diamond product development process</strong>. I enjoy working in all four stages.</p>
 		</div>
-		<figure>
-			<img src="" alt="double diamond product development process">
+		<figure class="my6">
+			<img src="/assets/images/process.svg" width="1000" height="470" alt="my product development process">
 		</figure>
-		<ul>
+		<ol>
 			<li class="mb4"><strong>Discover:</strong> Understand the current condition by auditing existing designs, reviewing competitors, interviewing users, “How Might We” brainstorming sessions, and gathering insights.</li>
 			<li class="mb4"><strong>Define:</strong> Synthesize the possibilities identified in the discovery phase and focuses on the most compelling opportunities to pursue. Which matters most? Which should we act on first? What is feasible? The goal is to understand exactly what the project will and won't do, and what "success" will look like.</li>
 			<li class="mb4"><strong>Develop:</strong> Explore the best potential solutions. Designs concepts are created, prototyped, tested, and iterated. This trial and error process helps ideas improve.</li>
 			<li class="lh-md"><strong>Deliver:</strong> As confidence in the solution increases, exploration gives way to engineering. Working software is created, optimized, and shipped.</li>
-		</ul>
+			<li class="lh-md"><strong>Improve:</strong> Measure results and tweaks where and when it's necessary.</li>
+		</ol>
 	</section>
 	<section>
 		<hr role="presentation" aria-role="hidden" class="hr hr-sm mb5 bg-violet-2">
