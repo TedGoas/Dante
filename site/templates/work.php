217d6cb088fd0590a6eac95ee28fc081026ae541
Ted Goas <ted@tedgoas.com>
2018-09-18 21:27:06 -0400

    Work Index images and copy tweaks

--- site/templates/work.php
+++ site/templates/work.php
@@ -5,55 +5,54 @@
 
 	<section class="mt5">
 		<h2 class="tiny">Currently</h2>
-		<p>I work at Stack Overflow creating better workflows for over 50 million developers.</p>
+		<p>I work at Stack Overflow creating better workflows for over 50 million&nbsp;developers.</p>
 		<div class="flex jc-space-between ai-start gs5 gsx sm--fd-column">
 			<a href="/work/stack-overflow" class="card card-work flex-cell grid--cell6">
 				<img src="/assets/images/work/stack-overflow/cover.jpg" alt="stack overflow logo above code" height="200" width="450">
 				<h3>Working @ Stack</h3>
-				<p>How we design at Stack Overflow and a few notable projects.</p>
+				<p>How we design at Stack Overflow and a few notable&nbsp;projects.</p>
 			</a>
 			<a href="/work/stack-overflow-for-enterprise" class="card card-work flex-cell grid--cell6">
 				<img src="https://via.placeholder.com/450x200">
 				<h3>Stack Overflow for Enterprise</h3>
-				<p>How we design Stack Overflow for large teams.</p>
+				<p>How we design Stack Overflow for large&nbsp;teams.</p>
 			</a>
 		</div>
 	</section>
 	<hr class="hr-sm">
 	<section class="mt5">
 		<h2 class="tiny">Before that</h2>
-		<p>I worked at Canfield Scientific designing applications that help doctors run their practice and clinicians run their studies.</p>
+		<p>I worked at Canfield Scientific designing applications that help doctors run their practice and clinicians run their&nbsp;studies.</p>
 		<div class="flex jc-space-between ai-start gs5 gsx sm--fd-column">
 			<a href="/work/canfield-scientific" class="card card-work flex-cell grid--cell6">
-				<img src="https://via.placeholder.com/450x200">
+				<img src="/assets/images/work/canfield/th-canfield.jpg" alt="model and canfield logo" height="200" width="450">
 				<h3>Working @ Canfield</h3>
-				<p>How we designed at Canfield and a few notable projects.</p>
+				<p>How we designed at Canfield and a few notable&nbsp;projects.</p>
 			</a>
 			<a href="/work/canfield-scientific-clinical-services" class="card card-work flex-cell grid--cell6">
 				<img src="https://via.placeholder.com/450x200">
 				<h3>Clinical Services</h3>
-				<p>How we created a clinical web app for pharma companies to organize trials.</p>
+				<p>How we created a clinical web app for pharmaceutical companies to organize&nbsp;trials.</p>
 			</a>
 		</div>
 	</section>
 	<hr class="hr-sm">
 	<section class="mt5">
 		<h2 class="tiny">Off Hours</h2>
-		<p>I maintain <a href="http://tedgoas.github.io/Cerberus/" class="color">Cerberus</a> and help folks with emails:</p>
+		<p>I usually cherish time away from the computer on night, but of course there are&nbsp;exceptions.</p>
 		<div class="flex jc-space-between ai-start gs5 gsx sm--fd-column">
 			<a href="/work/email-design" class="card card-work flex-cell grid--cell6">
 				<img src="https://via.placeholder.com/450x200">
 				<h3>Freelance Email Design</h3>
-				<p>How we designed at Canfield and a few notable projects.</p>
+				<p>Occasionally I help folks design and code HTML&nbsp;emails.</p>
 			</a>
-			<a href="/work/email-design" class="card card-work flex-cell grid--cell6">
-				<img src="https://via.placeholder.com/450x200">
+			<a href="https://tedgoas.github.io/Cerberus/" class="card card-work flex-cell grid--cell6">
+				<img src="/assets/images/work/th-cerberus.jpg" alt="Cerberus open source email templates" height="200" width="450">
 				<h3>Cerberus (Open Source)</h3>
-				<p>How we designed at Canfield and a few notable projects.</p>
+				<p>I created and actively maintain a set of responsive email patterns. ~3K stars on&nbsp;Github.</p>
 			</a>
 		</div>
 	</section>
 
 </div>
-
 <?php snippet('page-end') ?>
