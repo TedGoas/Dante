55c4c3eff07a47389ffc3b7bbf25f593856814f9
Ted Goas <ted@tedgoas.com>
2018-09-19 23:29:10 -0400

    Figures for Clinical Services

--- site/templates/work.php
+++ site/templates/work.php
@@ -9,7 +9,7 @@
 		<div class="flex jc-space-between ai-start gs5 gsx sm--fd-column">
 			<a href="/work/stack-overflow" class="card card-work flex-cell grid--cell6">
 				<img src="/assets/images/work/stack-overflow/cover.jpg" alt="stack overflow logo above code" height="200" width="450">
-				<h3>Working @ Stack</h3>
+				<h3>Working @ Stack Overflow</h3>
 				<p>How we design at Stack Overflow and a few notable&nbsp;projects.</p>
 			</a>
 			<a href="/work/stack-overflow-for-enterprise" class="card card-work flex-cell grid--cell6">
@@ -30,7 +30,7 @@
 				<p>How we designed at Canfield and a few notable&nbsp;projects.</p>
 			</a>
 			<a href="/work/canfield-scientific-clinical-services" class="card card-work flex-cell grid--cell6">
-				<img src="https://via.placeholder.com/450x200">
+				<img src="/assets/images/work/canfield-clinical-services/th-canfield-clinical-services.jpg" alt="model and canfield logo" height="200" width="450">
 				<h3>Clinical Services</h3>
 				<p>How we created a clinical web app for pharmaceutical companies to organize&nbsp;trials.</p>
 			</a>
