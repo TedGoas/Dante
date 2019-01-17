5ae0cf83b2b093610c6d0de3e6a64cf3d78a8315
Ted Goas <ted@tedgoas.com>
2019-01-16 21:34:45 -0500

    Revert "Sketchy highlight background"
    This reverts commit 2be2846849fffd914daf665400d51c77fae2bfb3.

--- site/templates/contact.php
+++ site/templates/contact.php
@@ -3,8 +3,8 @@
 	<div class="default-title">
 		<h1><?php echo $page->title()->html() ?></h1>
 	</div>
-	<p>If you're interested in <strong class="mark">freelance work</strong> and have something small or well-defined, let's chat. Please be aware that I already have a handful of freelance clients, personal projects, and my day job.</p>
-	<p>If you have a <strong class="mark2">question about Cerberus</strong>, consider opening an issue, <a href="https://twitter.com/TedGoas" rel="external me">pinging me on Twitter</a>, or emailing me.</p>
+	<p>If you're interested in <strong class="highlighted">freelance work</strong> and have something small or well-defined, let's chat. Please be aware that I already have a handful of freelance clients, personal projects, and my day job.</p>
+	<p>If you have a <strong class="highlighted">question about Cerberus</strong>, consider opening an issue, <a href="https://twitter.com/TedGoas" rel="external me">pinging me on Twitter</a>, or emailing me.</p>
 	<p>For anything else, shoot me an email!</p>
 	<p>
 		<img src="/assets/images/email.png" alt="My email address." width="330" height="66">
