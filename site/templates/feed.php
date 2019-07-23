902cce6b69b11f4a04176d1df8d91b8f45d1a553
Ted Goas <ted@tedgoas.com>
2019-07-22 20:34:29 -0400

    Delete Kirby

--- site/templates/feed.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-echo page('blog')->children()->visible()->flip()->limit(10)->feed(array(
-  'title'       => $page->title(),
-  'description' => $page->description(),
-  'link'        => 'blog',
-));
-
-?>
