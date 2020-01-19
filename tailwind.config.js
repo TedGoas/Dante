664255c79915ec801949fef1a108a9c9fa239a5a
Ted Goas <ted@tedgoas.com>
2020-01-18 20:46:24 -0500

    Convert to Max-width breakpoints
    https://tailwindcss.com/docs/breakpoints#max-width-breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,22 @@
 // tailwind.config.js
 module.exports = {
   theme: {
+    screens: {
+      'sm': {'max': '639px'},
+      // => @media (max-width: 639px) { ... }
+
+      'md': {'max': '767px'},
+      // => @media (max-width: 767px) { ... }
+
+      'lg': {'max': '1023px'},
+      // => @media (max-width: 1023px) { ... }
+
+      'xl': {'max': '1279px'},
+      // => @media (max-width: 1279px) { ... }
+    },
     fontFamily: {
       sans: ['arial', 'sans-serif'],
-      serif: ['Merriweather', 'serif'],
+      serif: ['serif'],
       mono: ['Roboto Mono', 'monospace'],
       display: ['Rock Salt', 'sans-serif'],
     },
