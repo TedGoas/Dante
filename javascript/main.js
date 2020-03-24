bec26793521a099f48005d72b37978aaf0e081e4
Ted Goas <ted@tedgoas.com>
2020-03-23 21:04:54 -0400

    Update to Skeleventy v2, now using Laravel Mix (Webpack)

--- javascript/main.js
+++ javascript/main.js
@@ -5,21 +5,6 @@
 // const $header = document.getElementById('#header')
 // const $nav = document.getElementById('#nav')
 // const $footer = document.getElementById('#footer')
-// Helpers
-var
-/**
-* @description Test if the body id is something
-* @param  		{string}
-* @return 		{bool}
-*
-*/
-page = function page(name) {
-  if (!name) {
-    return $body.getAttribute('id');
-  }
-
-  return $body.getAttribute('id') == name;
-};
 // add a class of 'intro' to the first paragraph
 // do this on every page apart from the blog page
 if (!page('blog')) {
