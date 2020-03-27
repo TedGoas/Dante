e36905f696ae35e01bd0a2e276806951edae546c
Ted Goas <ted@tedgoas.com>
2020-03-27 08:40:05 -0400

    🌟 Put my stuff back

--- filters/dates.js
+++ filters/dates.js
@@ -29,5 +29,5 @@ module.exports = function(date, part) {
     23 : "rd",
     31 : "st"
   };
-  return d.getDate() + (ordinal[d.getDate()] || "th") + " " + month[d.getMonth()] + ", " + d.getUTCFullYear();
+  return month[d.getMonth()] + " " + d.getDate() + (ordinal[d.getDate()] || "th") + ", " + d.getUTCFullYear();
 }
