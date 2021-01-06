e74cc853e6a7961de6ad6c5e296e4b79567572dd
Ted Goas <ted@tedgoas.com>
2021-01-05 20:52:13 -0500

    Make components for Header and Footer

---
+++ dante/src/components/Header.vue
@@ -0,0 +1,25 @@
+<template> 
+    <header class="header" role="banner">
+        <strong>
+        <g-link to="/"><g-image src="~/assets/img/tg-sig-logo.svg" :alt="$static.metadata.siteName"/></g-link>
+        </strong>
+        <nav role="navigation" aria-label="Main Navigation">
+        <g-link to="/work/">Works</g-link>
+        <g-link to="/values/">Values</g-link>
+        <g-link to="/bio/">Bio</g-link>
+        <g-link to="/articles/">Articles</g-link>
+        </nav>
+        <g-image src="~/assets/img/tg-sig-email.svg" alt="ted at tedgoas dot com"/>
+    </header>
+</template>
+<static-query>
+query {
+  metadata {
+    siteName
+  }
+}
+</static-query>
+
+<style lang="scss">
+
+</style>
