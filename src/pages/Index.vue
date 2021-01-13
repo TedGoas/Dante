01f4c637d00d520489d271afe0b4ef6fa2e4c1a9
Ted Goas <ted@tedgoas.com>
2021-01-12 21:04:39 -0500

    Create work list snippet HTML

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -7,35 +7,19 @@
         <p>I believe design is about taking the complex and chaotic and making it <strong class="highlight-yellow">accessible</strong> to people, <strong class="highlight-yellow">responsive</strong>, and as <strong class="highlight-yellow">fast</strong> as possible.</p>
         <p>My goal is to create work that’s cool enough to show <span class="tooltip">my friends</span> and honest enough to show <span class="tooltip">my parents</span>.</p>
       </section>
-      <section>
-        <div>
-          <h2>Dialpad</h2>
-          <p>Currently I do this at Dialpad, where I work on <g-link to="">internet phone and video tools</g-link>, our <a href="https://dialpad.design/">design system</a>, and the company’s remote working culture.</p>
-        </div>
-      </section>
-      <section>
-        <div>
-          <h2>Stack Overflow</h2>
-          <p>Before that I worked at Stack Overflow, creating better workflows for over 50 million developers. I’m particularly proud of the <g-link to="">community health dashboard</g-link> and Stack Overflow’s <a href="https://stackoverflow.design/email">email design system</a>.</p>
-        </div>
-      </section>
-      <section>
-        <div>
-          <h2>Canfield</h2>
-          <p>And before that I worked at Canfield Scientific, designing products to help doctors run their practice and clinicians run their studies. I’m particularly proud of the <g-link to="">clinical services app</g-link> I worked on.</p>
-        </div>
-      </section>
-      <section>
-        <div>
-          <p>I enjoy writing about things I learn, speaking occasionally, and maintaining Cerberus (OSS).
-  When not in front of a screen I like to exercise, snowboard, both watch and play hockey and soccer, work on my house, and explore New Jersey with my wife and kids.</p>
-        </div>
-      </section>
+      <WorkList />
     </div>
   </Layout>
 </template>
 
 <script>
+import WorkList from "@/components/WorkList";
+
+export default {
+  components: {
+    WorkList
+  }
+}
 </script>
 
 <style>
