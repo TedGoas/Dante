a5e871aec666055b0e92486193feb7bf24b7e6ca
Ted Goas <ted@tedgoas.com>
2021-03-08 20:49:35 -0500

    Added a few images and started styling the header but I want SASS

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,43 +16,16 @@ module.exports = {
       xl: '1280px'
     },
     colors: {
+      sitebg: '#0E091D',
       transparent: 'transparent',
       current: 'currentColor',
       black: colors.black,
       white: colors.white,
-      gray: {
-        100: '#f7fafc',
-        200: '#edf2f7',
-        300: '#e2e8f0',
-        400: '#cbd5e0',
-        500: '#a0aec0',
-        600: '#718096',
-        700: '#4a5568',
-        800: '#2d3748',
-        900: '#1a202c'
-      },
+      gray: colors.blueGray,
       teal: colors.teal
     },
     extend: {
-      typography: (theme) => ({
-        DEFAULT: {
-          css: {
-            'h1': {
-              fontWeight: '700'
-            },
-            'blockquote': {
-              fontWeight: 'normal',
-              color: theme('colors.gray.700')
-            },  
-            'blockquote p:first-of-type::before': {
-              content: ''
-            },
-            'blockquote p:last-of-type::after': {
-              content: ''
-            }
-          }
-        }
-      })
+
     },
   },
   plugins: [
