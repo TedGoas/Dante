8139fd82e5ec763e9e42e77ee68250a27f578e59
Ted Goas <ted@tedgoas.com>
2021-03-11 20:48:26 -0500

    Finalize footer

--- .eleventy.js
+++ .eleventy.js
@@ -10,6 +10,7 @@ module.exports = (config) => {
   config.addPassthroughCopy('src/assets/img/');
   config.addPassthroughCopy({ 'src/posts/img/': 'assets/img/' });
   config.addPassthroughCopy({ 'src/work/img/': 'assets/img/' });
+  config.addPassthroughCopy('src/assets/files/');
 
   config.addWatchTarget("src/assets/js/");
 
