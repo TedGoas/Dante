249efe54af7bac731fb6fb5d6551de1956f56e91
Ted Goas <ted@tedgoas.com>
2021-03-13 17:31:56 -0500

    Style opening typography on homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,19 @@ module.exports = {
       'mono': ['ui-monospace', 'SFMono-Regular'],
       'display': ['"Caveat Brush"', 'Oswald']
     },
+    fontSize: {
+      'xs': '.875rem',
+      'sm': '1rem',
+      'base': '1.2rem',
+      'lg': '1.25rem',
+      'xl': '1.5rem',
+      '2xl': '1.875rem',
+      '3xl': '2.125rem',
+      '4xl': '2.5rem',
+      '5xl': '3.5rem',
+      '6xl': '4.5rem',
+      '7xl': '5.5rem'
+      },
     colors: {
       sitebg: '#0E091D',
       transparent: 'transparent',
